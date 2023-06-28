e4c6541197d19c9056481d81d86d4af256f81bcf
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2023-06-28 12:18:17 +0200

    Finish js function at mod 5.3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# 5.javascript-blog
-Kodilla module 5
+CZEŚĆ PATRYK. 
+W kodzie script.js dodałem pytanie do Ciebie. 
+Moj kod działa dobrze ale nie jestem pewien która wersja jest lepsza. 
+Pozdr
--- js/script.js
+++ js/script.js
@@ -1,36 +1,64 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log('Link was clicked!');
-  console.log(event);
+  event.preventDefault();
+  const clickedElement = this;
+  //console.log('Link was clicked!');
+  //console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
-
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  //console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+  //console.log('clickedElement (with plus): ' + clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts article.active');
-
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  const hrefAtribute = clickedElement.getAttribute("href");
+  //console.log(hrefAtribute);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] add class 'active' to the correct article */
+
+  const allArticles = document.querySelectorAll('.posts article');
+  for(let singleArticle of allArticles){
+    //console.log(singleArticle.getAttribute("id"));
+    if (("#" + singleArticle.getAttribute("id")) == hrefAtribute) {
+      singleArticle.classList.add('active');
+    }
+  }
+
+  /* PYTANIE DO MENTORA !!! :)
+  W podpowiedziach, zadanie, jest ogarnięte tak:
+  
+  const targetArticle = document.querySelector(hrefAtribute);
+  targetArticle.classList.add('active');
+  
+  Ja sam zrobiłem to bez podpowiedzi, z wykorzystaiem pętli for, tak jak w aktywnym kodzie powyżej.
+  Musiałem dodać "#" do atrybutu id podczas porwnywania. 
+  Inaczej elementy nie były identyczne i blok if się nie wykonywał.
+
+  Oba sposoby działają. Który jest lepszy? Bardziej pożądany? 
+  Ten z podpowiedzi jest krótszy i prostszy. Czyli wg zasady Keep It Simple Stupid byłby lepszy ? :)
+  */
+
 }
 
 const links = document.querySelectorAll('.titles a');
-
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
